2f6a64b6f35a715699251be6f5ca210ed4fcc807
Matheus Gabriel <matheusg663@gmail.com>
2022-06-27 21:31:59 -0300

    Manipulando um arquivo

---
+++ manipulando_um_arquivo/teste.js
@@ -0,0 +1,11 @@
+// Isso é um comentario de uma linha
+
+/* 
+Isso é 
+um  de 
+várias linhas
+*/
+
+var desconto = 0.02; // Valores com var pode ser alterado
+const PRECO = 2; // Valores com const não podem ser alteradas
+var total = PRECO - desconto;
