08d0f377cfc98935bc68d0dc615de2c7e506347e
Matheus Gabriel <matheusg663@gmail.com>
2022-06-30 21:19:13 -0300

    Constante

---
+++ constante/index.js
@@ -0,0 +1,7 @@
+const FIRST_NAME = 'Stephany'; //Vai da certo
+
+FIRST_NAME = 'Stephany';
+
+console.log(FIRST_NAME);
+
+// const FIRST_NAME; //Vai dar erro, porque variaveis do tipo const precisam ser inicilizadas antes de terem uma atribuição 
