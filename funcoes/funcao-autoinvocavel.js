95ffb840026d723ae46c488d17c722931a008a2d
Matheus Gabriel <matheusg663@gmail.com>
2022-07-02 16:08:15 -0300

    Funções Anonimas, Callbacks e autoinvocável

---
+++ funcoes/funcao-autoinvocavel.js
@@ -0,0 +1,7 @@
+const soma = (
+    function (a, b) {
+        return a + b;
+    }(1, 2)
+)
+
+console.log(soma);
---
+++ funcoes/funcao-variavel.js
@@ -0,0 +1,6 @@
+const soma = function (a,b) {
+    return a + b;
+}
+
+console.log(soma);
+console.log(soma(1,2));
