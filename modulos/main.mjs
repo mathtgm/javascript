26240e66a300517908d588fa405bc7b60ad64c2c
Matheus Gabriel <matheusg663@gmail.com>
2022-07-06 23:27:06 -0300

    Módulos JS

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostrarHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Matheus', 24));
+console.log(mostraCidade('Matheus', 'Araras'));
+console.log(mostrarHobby('Matheus', 'Jogar'));
